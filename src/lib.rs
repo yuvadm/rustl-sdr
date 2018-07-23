b19f35cbc702f3855db52824e26939093148f1b9
Yuval Adam <_@yuv.al>
2018-07-23 19:56:08 +0300

    Initial RtlSdr struct and impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,36 @@
+extern crate libusb;
+
 pub struct Dongle {
     vendor: u16,
     product: u16,
     name: &'static str
 }
+
+pub struct RtlSdr {
+    usb_ctx: libusb::Context
+}
+
+impl RtlSdr {
+    pub fn new() -> RtlSdr {
+        let usb_ctx = libusb::Context::new().unwrap();
+        RtlSdr {
+            usb_ctx
+        }
+    }
+
+    pub fn find(&self) {
+        for mut dev in self.usb_ctx.devices().unwrap().iter() {
+            let desc = dev.device_descriptor().unwrap();
+            println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
+                dev.bus_number(),
+                dev.address(),
+                desc.vendor_id(),
+                desc.product_id());
+        }
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 extern crate libusb;
 
+mod lib;
+
+use lib::RtlSdr;
+
 fn main() {
     println!("Running");
-    let ctx = libusb::Context::new().unwrap();
-    for mut dev in ctx.devices().unwrap().iter() {
-        let desc = dev.device_descriptor().unwrap();
-        println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
-            dev.bus_number(),
-            dev.address(),
-            desc.vendor_id(),
-            desc.product_id());
-    }
+    let rtlsdr = RtlSdr::new();
+    rtlsdr.find();
 }
